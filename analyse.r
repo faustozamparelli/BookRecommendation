bf14e49c7af859830554e95b59316eeb2f414097
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2024-06-12 18:52:58 +0200

    add analyse.r

---
+++ analyse.r
@@ -0,0 +1,6 @@
+# sample
+book_tags <- read.csv("https://github.com/zygmuntz/goodbooks-10k/raw/master/samples/book_tags.csv")
+books <- read.csv("https://github.com/zygmuntz/goodbooks-10k/raw/master/samples/books.csv")
+ratings <- read.csv("https://github.com/zygmuntz/goodbooks-10k/raw/master/samples/ratings.csv")
+tags = read.csv("https://github.com/zygmuntz/goodbooks-10k/raw/master/samples/tags.csv")
+to_read <- read.csv("https://github.com/zygmuntz/goodbooks-10k/raw/master/samples/to_read.csv")
