379323ab647cc05bfb61d806b7a2df4ded231507
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2024-06-13 12:39:24 +0200

    play around

--- analyse.r
+++ analyse.r
@@ -1,32 +1,38 @@
 library(igraph)
+library(dplyr)
+library(data.table)
 
 options(timeout = max(1000, getOption("timeout")))
 
-book_tags <- read.csv(
+book_tags <- data.table(read.csv(
   "https://github.com/zygmuntz/goodbooks-10k/raw/master/book_tags.csv"
-)
-books <- read.csv(
+))
+books <- data.table(read.csv(
   "https://github.com/zygmuntz/goodbooks-10k/raw/master/books.csv"
-)
-ratings <- read.csv(
-  "https://github.com/zygmuntz/goodbooks-10k/raw/master/ratings.csv"
-)
-tags <- read.csv(
+))
+ratings <- data.table(read.csv(
+  # "https://github.com/zygmuntz/goodbooks-10k/raw/master/ratings.csv"
+  "~/Developer/StartupAnalysis/ratings.csv"
+))
+tags <- data.table(read.csv(
   "https://github.com/zygmuntz/goodbooks-10k/raw/master/tags.csv"
-)
-to_read <- read.csv(
+))
+to_read <- data.table(read.csv(
   "https://github.com/zygmuntz/goodbooks-10k/raw/master/to_read.csv"
-)
+))
 
 # new bool column "liked_it" TRUE for ratings >= 4
 ratings$liked_it <- ifelse(ratings$rating >= 4, TRUE, FALSE)
 
-# merge ratings with book name and author
-ratings <- merge(ratings, books, by = "book_id")[
-  c("book_id", "user_id", "liked_it", "rating", "original_title")
-]
-
 head(ratings)
+head(books)
+
+# merge ratings with book name and author
+augmented_ratings <- merge(ratings, books, by = "book_id") %>%
+  select(
+    "book_id", "user_id", "liked_it", "rating", "original_title"
+  )
+head(augmented_ratings)
 
 # length of ratings
 nrow(ratings)
@@ -37,6 +43,24 @@ table(ratings$rating) / nrow(ratings)
 # top 10 with highest average rating
 
 # what influences a book's rating?
+# 1. Relationship between rating and number of ratings
+head(ratings)
+cor(ratings$rating, ratings$rating_count)
+
+# 2. Relationship between rating and average rating of the author's other books
+author_avg_rating <- aggregate(rating ~ author, data = ratings, FUN = mean)
+cor(ratings$rating, author_avg_rating$rating)
+
+# 3. Relationship between rating and book length (number of pages)
+cor(ratings$rating, books$num_pages)
+
+# 4. Relationship between rating and book publication year
+cor(ratings$rating, books$publication_year)
+
+# 5. Relationship between rating and book genre
+genre_ratings <- merge(ratings, book_tags, by = "book_id")
+genre_avg_rating <- aggregate(rating ~ tag_name, data = genre_ratings, FUN = mean)
+cor(ratings$rating, genre_avg_rating$rating)
 
 # is there correlation between average rating and number of ratings?
 
